4cb25f1b9debef303387a4c104b2cfb8511aafd6
singharvindbisht <arvind.singh7112000@gmail.com>
2022-04-27 11:04:19 +0530

    get user using name

--- src/main/java/com/admin/repository/AdminRepo.java
+++ src/main/java/com/admin/repository/AdminRepo.java
@@ -2,8 +2,14 @@ package com.admin.repository;
 
 import com.admin.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminRepo extends JpaRepository<Admin,Long> {
+	
+	@Query(" select ar from AdminRepo ar"+
+			" where ar.adminUsername = :username ")
+	Admin findByUsername(@Param("username") String username);
 }
