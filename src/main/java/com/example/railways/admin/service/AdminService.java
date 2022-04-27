2362b216007aa5f40c660f2cb4e5295270b36c6b
Thryambak <73072340+Thryambak@users.noreply.github.com>
2022-04-27 13:25:07 +0530

    Add files via upload

---
+++ src/main/java/com/example/railways/admin/service/AdminService.java
@@ -0,0 +1,12 @@
+package com.example.railways.admin.service;
+
+import java.util.ArrayList;
+
+import com.example.railways.admin.model.AdminDetails;
+
+public interface AdminService {
+
+    public AdminDetails createAdmin(AdminDetails admin);
+
+	public ArrayList<AdminDetails> getAdmins();
+}
---
+++ src/main/java/com/example/railways/admin/serviceImpl/AdminServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.railways.admin.serviceImpl;
+
+import com.example.railways.admin.model.AdminDetails;
+import com.example.railways.admin.repository.AdminRepo;
+import com.example.railways.admin.service.AdminService;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+
+
+    @Autowired
+    AdminRepo adminRepo;
+    @Override
+    public AdminDetails createAdmin(AdminDetails admin) {
+        adminRepo.save(admin);
+        return admin;
+    }
+	@Override
+	public ArrayList<AdminDetails> getAdmins() {
+		// TODO Auto-generated method stub
+		return  (ArrayList<AdminDetails>) adminRepo.findAll();
+	}
+	
+}
