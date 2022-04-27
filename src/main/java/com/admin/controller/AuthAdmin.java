8babeefa287604dd318075a98a3e609f2b28d15c
Thryambak <thryambakmv@gmail.com>
2022-04-27 10:07:51 +0530

    Admin

--- src/main/java/com/admin/controller/AuthAdmin.java
+++ src/main/java/com/admin/controller/AuthAdmin.java
@@ -1,4 +1,21 @@
 package com.admin.controller;
 
+import com.admin.model.Admin;
+import com.admin.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthAdmin {
+
+    @Autowired
+    AdminService adminService;
+    @PostMapping("createAdmin")
+    public Admin createAdmin(@RequestBody Admin admin){
+       return adminService.createAdmin(admin);
+    }
+
+
 }
---
+++ src/main/java/com/admin/service/AdminService.java
@@ -0,0 +1,8 @@
+package com.admin.service;
+
+import com.admin.model.Admin;
+
+public interface AdminService {
+
+    public Admin createAdmin(Admin admin);
+}
