8c00fc51a9f52df7fa42e29a455c66a2e8632508
Thryambak <thryambakmv@gmail.com>
2022-04-27 10:58:18 +0530

    Added response

--- src/main/java/com/admin/controller/AuthAdmin.java
+++ src/main/java/com/admin/controller/AuthAdmin.java
@@ -1,8 +1,15 @@
 package com.admin.controller;
 
 import com.admin.model.Admin;
+import com.admin.response.AdminDetailsResponse;
 import com.admin.service.AdminService;
+
+import java.util.Date;
+
+import org.aspectj.weaver.NewConstructorTypeMunger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +20,14 @@ public class AuthAdmin {
     @Autowired
     AdminService adminService;
     @PostMapping("createAdmin")
-    public Admin createAdmin(@RequestBody Admin admin){
-       return adminService.createAdmin(admin);
-    }
+    public ResponseEntity<Object> createAdmin(@RequestBody Admin admin){
+       
+    	
+    	
+   return new ResponseEntity<Object>( new AdminDetailsResponse(new Date(),"Admin added Successfully",adminService.createAdmin(admin)),HttpStatus.OK);
+   
+    
+   }
 
 
 }
---
+++ src/main/java/com/admin/response/AdminDetailsResponse.java
@@ -0,0 +1,43 @@
+package com.admin.response;
+
+import java.util.Date;
+
+import com.admin.model.Admin;
+
+public class AdminDetailsResponse {
+
+	private Date date;
+	private String messageString;
+	private Admin admin;
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getMessageString() {
+		return messageString;
+	}
+	public void setMessageString(String messageString) {
+		this.messageString = messageString;
+	}
+	public Admin getAdmin() {
+		return admin;
+	}
+	public void setAdmin(Admin admin) {
+		this.admin = admin;
+	}
+	public AdminDetailsResponse(Date date, String messageString, Admin admin) {
+		super();
+		this.date = date;
+		this.messageString = messageString;
+		this.admin = admin;
+	}
+	public AdminDetailsResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
