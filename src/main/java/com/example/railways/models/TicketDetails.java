17116f022dea284ac0610e9bf7e50f5a14f572af
Ruchira-R <ruchvad@gmail.com>
2022-04-28 12:43:51 +0530

    cleaned imports

--- src/main/java/com/example/railways/models/TicketDetails.java
+++ src/main/java/com/example/railways/models/TicketDetails.java
@@ -1,14 +1,8 @@
 package com.example.railways.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity
 @Table(name = "tickets")
 public class TicketDetails {
