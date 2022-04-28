d0f458dc8292cf86708020883b44dd7f8efaceaa
Ruchira-R <ruchvad@gmail.com>
2022-04-28 09:37:52 +0530

    ticket impl annotation

--- src/main/java/com/example/railways/serviceimpl/TicketServiceImpl.java
+++ src/main/java/com/example/railways/serviceimpl/TicketServiceImpl.java
@@ -7,7 +7,9 @@ import com.example.railways.repository.TicketRepository;
 import com.example.railways.service.TicketService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TicketServiceImpl implements TicketService{
     @Autowired
     TicketRepository ticketRepository;
