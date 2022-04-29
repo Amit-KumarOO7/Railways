9a82b4c5353cd07739297678038f408bd65786fd
Chandan <ckumar@zscaler.com>
2022-04-29 12:10:48 +0530

    minor fixes payments

--- src/main/java/com/example/railways/repository/PaymentRepository.java
+++ src/main/java/com/example/railways/repository/PaymentRepository.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<PaymentModel, Long> {
-
-    @Query(value = "SELECT record FROM payments WHERE record.payment_id=:paymentID AND record.u_id=:uid", nativeQuery = true)
-    List<PaymentModel> getPaymentList(String paymentID, String uid);
+    @Query(value = "SELECT record FROM payments WHERE record.payment_id=?1 AND record.u_id=?2", nativeQuery = true)
+    List<PaymentModel> getPaymentList(String payment_id, String u_id);
 }
